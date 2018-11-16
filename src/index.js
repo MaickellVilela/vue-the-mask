59129ba4b5959b5c105aaa655878fe0c2e60c245
Igor Hideki <igor.hideki@before.com.br>
2018-11-16 15:58:21 -0300

    add export file masker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import tokens from './tokens'
 import mask from './directive'
+import masker from './masker'
 import TheMask from './component.vue'
 
 function install(Vue) {
@@ -8,7 +9,7 @@ function install(Vue) {
 }
 
 export default install
-export { TheMask, mask, tokens }
+export { TheMask, mask, masker, tokens }
 
 // Install by default if included from script tag
 if (typeof window !== 'undefined' && window.Vue) {
